61f2d88639b655db37830ba1cb70c966219b6c88
Fanil <nffsam@gmail.com>
2023-10-12 16:18:03 +0400

    hw3 fixed

--- src/main/java/ru/nuraev/homeworks/Main.java
+++ src/main/java/ru/nuraev/homeworks/Main.java
@@ -5,12 +5,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("START");
-        greetings();
-        checkSign(1, 2, -10);
-        selectColor(10);
-        compareNumbers(5, 10);
-        addOrSubtractAndPrint(10, 5, true);
-        scanner(1,2,3,4,5);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите метод запуска от 1 до 5");
+        int result = scanner.nextInt(); //не могу вот тут определиться как вызвать
+        if (result == 1) {
+            greetings();
+        }
+        if (result == 2) {
+            checkSign(1, 2, -10);
+        }
+        if (result == 3) {
+            selectColor(10);
+        }
+        if (result == 4) {
+            compareNumbers(5, 10);
+        }
+        if (result == 5) {
+            addOrSubtractAndPrint(10, 5, true);
+        }
+        System.out.println(result);
 
         System.out.println("END");
     }
@@ -35,11 +48,13 @@ public class Main {
             System.out.println("Желтый");
         if (color > 20) System.out.println("Зеленый");
     }
+
     public static void compareNumbers(int a, int b) {
         if (a >= b)
             System.out.println("a >= b");
         else System.out.println("a < b");
     }
+
     public static void addOrSubtractAndPrint(int initValue, int delta, boolean increment) {
         int res1 = initValue + delta;
         int res2 = initValue - delta;
@@ -48,12 +63,6 @@ public class Main {
         else System.out.println(res2);
     }
 
-    public static void scanner (int a, int b, int c, int d, int e) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите метод запуска от 1 до 5");
-        int result = scanner.nextInt(greetings();); //не могу вот тут определиться как вызвать
-                    System.out.println(result);
-    }
 
 }
 
